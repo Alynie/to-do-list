aeb7f7078a3afc4df2e132fcf7b7e66ef64cf392
Alynie <41819157+Alynie@users.noreply.github.com>
2025-11-17 22:44:15 -0600

    feat: add list and item (#3)

---
+++ src/components/TodoListItem.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface TodoListItemProps {
+    id: string;
+    text: string;
+    isComplete: boolean;
+}
+
+const TodoListItem: React.FC<TodoListItemProps> = ({text, isComplete}) => {
+    return (<div className="item">
+        <input type="checkbox" checked={isComplete}/>
+        <p>{text}</p>
+    </div>)
+};
+
+export default TodoListItem;
