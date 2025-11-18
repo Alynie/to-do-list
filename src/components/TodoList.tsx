eebd048fcc44e302c9c51d40385b579a7eb607d7
Alynie <alynie.walter@gmail.com>
2025-11-17 22:43:39 -0600

    add list and item

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React, {useState} from 'react';
+import TodoListItem from './TodoListItem';
+
+interface TodoListProps {
+
+}
+
+const TodoList: React.FC<TodoListProps> = () => {
+    const [tasks, setTasks] = useState([{id: '1', text: 'test', isComplete: false}, {id: '2', text: 'test2', isComplete: true}])
+    return (<div className='list'>{tasks.map(item => <TodoListItem key={item.id} id={item.id} text={item.text} isComplete={item.isComplete}/>)}</div>)
+}
+
+export default TodoList;
