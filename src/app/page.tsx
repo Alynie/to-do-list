a5181f6e2056787b84a379106638e22d2c8306c4
Alynie <alynie.walter@gmail.com>
2025-11-17 22:56:31 -0600

    chore: add eslint and prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import TodoList from "../components/TodoList";
 
 export default function Home() {
@@ -6,7 +5,7 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div>
-          <TodoList/>
+          <TodoList id={"1"} name={"Default"} />
         </div>
       </main>
     </div>
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,14 +1,29 @@
-'use client'
-import React, {useState} from 'react';
-import TodoListItem from './TodoListItem';
+"use client";
+import React, { useState } from "react";
+import TodoListItem from "./TodoListItem";
 
 interface TodoListProps {
-
+  id: string;
+  name: string;
 }
 
 const TodoList: React.FC<TodoListProps> = () => {
-    const [tasks, setTasks] = useState([{id: '1', text: 'test', isComplete: false}, {id: '2', text: 'test2', isComplete: true}])
-    return (<div className='list'>{tasks.map(item => <TodoListItem key={item.id} id={item.id} text={item.text} isComplete={item.isComplete}/>)}</div>)
-}
+  const [tasks, setTasks] = useState([
+    { id: "1", text: "test", isComplete: false },
+    { id: "2", text: "test2", isComplete: true },
+  ]);
+  return (
+    <div className="list">
+      {tasks.map((item) => (
+        <TodoListItem
+          key={item.id}
+          id={item.id}
+          text={item.text}
+          isComplete={item.isComplete}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default TodoList;
